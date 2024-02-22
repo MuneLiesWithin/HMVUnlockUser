30c198056195ef4487a791cfce068dfabb8791d1
MuneLiesWithin <romulo_medeiros@hotmail.com>
2024-02-22 08:56:55 -0300

    final unlock

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
-function unlock()
-{
-    let input = document.getElementById("username").value
-    if(input.trim() == ""){
-        flashmessage("neutral", "Por favir insira um usuário")
+function unlockUser() {
+    let usernameUnlock = document.getElementById("usernameUnlock").value
+    if(usernameUnlock.trim() == ""){
+        flashMessage("neutral", "Por favor insira um usuário")
     } else {
         fetch('https://localhost:44328/api/UnlockUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ username: input }),
+            body: JSON.stringify({ username: usernameUnlock }),
         })
         .then(res =>   res.json())
         .then(data => {
             if(data === "Usuário desbloqueado") 
             {
-                flashmessage("success", data)
+                flashMessage("success", data)
             }
             else 
             {
-                flashmessage("error", data)
+                flashMessage("warning", data)
             }
             
         })
         .catch(error => {
-            flashmessage("error", "Ocorreu algum erro")
+            flashMessage("error", "Ocorreu algum erro")
             console.error(error)
         })
     }
 }
-function flashmessage(type, message)
-{
-    let messageElement = document.getElementById("message") 
 
-    if(type === "error") {
-       messageElement.classList.add("error")
-       messageElement.innerHTML = message
-       setTimeout(() => {
+function flashMessage(type, message) {
+    const messageElement = document.getElementById("message");
+    messageElement.classList.add(type);
+    messageElement.innerHTML = message;
+    setTimeout(() => {
         messageElement.innerHTML = "";
-        messageElement.classList.remove("error");
-    }, 3000)
+        messageElement.classList.remove(type);
+    }, 3000);
+}
 
-    }
-    else if(type === "success") {
-        messageElement.classList.add("success")
-        messageElement.innerHTML = message
-        setTimeout(() => {
-            messageElement.innerHTML = "";
-            messageElement.classList.remove("success");
-        }, 3000)
-    }
-    else if(type === "neutral") {
-        messageElement.classList.add("neutral")
-        messageElement.innerHTML = message
-        setTimeout(() => {
-            messageElement.innerHTML = "";
-            messageElement.classList.remove("neutral");
-        }, 3000)
+function handleKeyPress(event, buttonId) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); 
+        document.getElementById(buttonId).click(); 
     }
 }
